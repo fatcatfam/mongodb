944ae0a9de4aaa59a40286a1b71fa9d6bf57376f
sam1el <jbrimager@chef.io>
2019-06-17 18:37:45 -0500

    changing the way the bin is loaded
    Signed-off-by: sam1el <jbrimager@chef.io>

--- plan.ps1
+++ plan.ps1
@@ -7,14 +7,10 @@ $pkg_license=@('AGPL-3.0')
 $pkg_source="https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-4.0.10-signed.msi"
 $pkg_shasum="d1ddac7ba6e2fbdfaaa0a787b83c165d4ad61795c051dc0f3142717a0b6a3707"
 $pkg_upstream_url='https://www.mongodb.com/'
-$pkg_bin_dirs=@('bin')
 $pkg_deps=@("core/lessmsi")
+$pkg_bin_dirs=@('MongoDB\Server\4.0\bin')
 $pkg_exports=@{port="mongod.net.port"}
 $pkg_exposes=@('port')
-
-function Invoke-SetupEnvironment {
-  Set-RuntimeEnv MONGO_HOME "$pkg_prefix\MongoDB\Server\4.0\bin" [-force]
-}
 
 function Invoke-Unpack {
   mkdir "$HAB_CACHE_SRC_PATH\$pkg_dirname"
@@ -26,7 +22,10 @@ function Invoke-Unpack {
 }
 
 function Invoke-Install {
-  Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\mongodb-*\SourceDir\*" "$pkg_prefix" -Recurse -Force
+Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\mongodb-*\SourceDir\*" "$pkg_prefix" -Recurse -Force -verbose
+# Set-Alias mongod "$pkg_prefix\MongoDB\Server\4.0\bin\mongod.exe"
+# Set-Alias mongo "$pkg_prefix\MongoDB\Server\4.0\bin\mongo.exe"
+mongod --version
 }
 
 function Invoke-Check {
