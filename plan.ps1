27ec4321595e70650594916721df17e5325c3cfe
sam1el <jbrimager@chef.io>
2019-06-17 15:44:17 -0500

    cleaning up run logic

--- plan.ps1
+++ plan.ps1
@@ -7,28 +7,29 @@ $pkg_license=@('AGPL-3.0')
 $pkg_source="https://fastdl.mongodb.org/win32/mongodb-win32-x86_64-2008plus-ssl-4.0.10-signed.msi"
 $pkg_shasum="d1ddac7ba6e2fbdfaaa0a787b83c165d4ad61795c051dc0f3142717a0b6a3707"
 $pkg_upstream_url='https://www.mongodb.com/'
-$pkg_deps=@("core/lessmsi", "core/python")
-$pkg_svc_run="mongod --config {{pkg_svc_config_path}}\mongod.conf"
+$pkg_bin_dirs=@('bin')
+$pkg_deps=@("core/lessmsi")
 $pkg_exports=@{port="mongod.net.port"}
 $pkg_exposes=@('port')
 
+function Invoke-SetupEnvironment {
+  Set-RuntimeEnv MONGO_HOME "$pkg_prefix\MongoDB\Server\4.0\bin" [-force]
+}
+
 function Invoke-Unpack {
-  mkdir "$HAB_CACHE_SRC_PATH/$pkg_dirname"
-  Push-Location "$HAB_CACHE_SRC_PATH/$pkg_dirname"
+  mkdir "$HAB_CACHE_SRC_PATH\$pkg_dirname"
+  Push-Location "$HAB_CACHE_SRC_PATH\$pkg_dirname"
   try {
-    lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH/$pkg_filename").Path
+    lessmsi x (Resolve-Path "$HAB_CACHE_SRC_PATH\$pkg_filename").Pathz
   }
   finally { Pop-Location }
 }
 
 function Invoke-Install {
-  Copy-Item "$HAB_CACHE_SRC_PATH/$pkg_dirname/mongodb-*/SourceDir/*" "$pkg_prefix" -Recurse -Force
-  Set-Alias mongod "$pkg_prefix\MongoDB\Server\4.0\bin\mongod.exe"
-  Set-Alias mongo "$pkg_prefix\MongoDB\Server\4.0\bin\mongo.exe"
-  mongod --version
+  Copy-Item "$HAB_CACHE_SRC_PATH\$pkg_dirname\mongodb-*\SourceDir\*" "$pkg_prefix" -Recurse -Force
 }
 
 function Invoke-Check {
-  & "$HAB_CACHE_SRC_PATH/$pkg_dirname/mongodb-win32-*/MongoDB/Server/4.0/bin/mongod" --version
+  & "$HAB_CACHE_SRC_PATH\$pkg_dirname\mongodb-win32-*\MongoDB\Server\4.0\bin/mongod" --version
   if($LASTEXITCODE -ne 0) { Write-Error "Invoke check failed with error code $LASTEXITCODE" }
 }
